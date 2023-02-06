c04fc57d39f5ebfe69c6e8e0f7d29458356e6af4
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-06 02:19:29 -0500

    update

---
+++ client/src/components/StudentList.js
@@ -0,0 +1,8 @@
+import React, {useState, userEffect} from "react";
+
+import axios from "axios"
+
+const StudentList = async () => {
+    const response = await axios.get("http://localhost:500/students/${id);
+    
+}
